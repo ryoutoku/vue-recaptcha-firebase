fe9c3d1442183ac7aa6da9d0160466d4ea6eca49
ryoutoku <invisible060@gmail.com>
2019-07-09 12:47:53 +0900

    feat: firebase設定追加

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import Vue from 'vue';
 import App from './App.vue';
 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+// ここにfirebaseの設定を追加
+firebase.initializeApp({
+  apiKey: '<api-key>',
+  authDomain: 'domain',
+  databaseURL: 'url',
+  projectId: 'project-id',
+  storageBucket: 'storage',
+  messagingSenderId: 'Id',
+  appId: 'appId',
+});
+
 Vue.config.productionTip = false;
 
 new Vue({
