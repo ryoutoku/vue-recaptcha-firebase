08244d33bba42df016d7dd18bd7461ecda8a6d58
ryoutoku <invisible060@gmail.com>
2019-07-09 12:47:05 +0900

    feat: backend側処理追加

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,26 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as rp from "request-promise";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const checkRecaptcha = functions.https.onCall(async (data, context) => {
+  const secret = "secret";
+
+  let response = {};
+  await rp({
+    uri: "https://recaptcha.google.com/recaptcha/api/siteverify",
+    method: "POST",
+    formData: {
+      secret,
+      response: data.token,
+    },
+    json: true,
+  })
+    .then(async result => {
+      console.log("recaptcha result", result);
+      response = result;
+    })
+    .catch(async reason => {
+      console.log("Recaptcha request failure", reason);
+      response = reason;
+    });
+  return response;
+});
